0c16be75a9a8352dfb2deb0b458d20cc8ef4d9a6
Arnaud Kervern <akervern@nuxeo.com>
2017-01-10 11:34:38 +0100

    Parse Perforce commit output and call Operation

--- perforce-triggers/change-commit.js
+++ perforce-triggers/change-commit.js
@@ -1,20 +1,110 @@
 #!/usr/bin/env node
-//Usage: $0 %change% %serverport%
-const tmp = '/tmp/perforce';
-const rand = Math.random().toString(36).slice(-10);
 
+'use strict';
+
+const _ = require('lodash');
+const Nuxeo = require('nuxeo');
+const s = require('synchronize');
 const exec = require('child_process').execSync;
+
+const P4CLIENT = process.env.P4CLIENT || '/Users/arnaud/Nuxeo/tmp/perforce/p4';
+const NUXEO_HOST = process.env.NUXEO_HOST || 'http://localhost:8080/nuxeo';
+const NUXEO_USER = process.env.NUXEO_USER || 'Administrator';
+const NUXEO_PWD = process.env.NUXEO_PWD || 'Administrator';
+const OPERATION = 'Document.VCSEventsReceiverOperation';
+
+//Usage: $0 %change% %serverport%
+//Sample Commande: $0 $change localhost:1666
+
+// Ensure args are correct
 if (process.argv.length < 4) {
   console.log(process.argv.join(', '));
   console.log('Usage: $0 %change% %serverport%');
   process.exit(1);
 }
 
-const args = process.argv.splice(2);
-const change = args[0];
-const server = args[1];
-const res = exec(`/Users/arnaud/Nuxeo/tmp/perforce/p4 -p ${server} describe -s ${change}`);
+// Ensure Nuxeo Client can authenticate
+const nuxeo = new Nuxeo({
+  baseURL: NUXEO_HOST,
+  auth: {
+    method: 'basic',
+    username: NUXEO_USER,
+    password: NUXEO_PWD
+  }
+});
 
-exec(`set > ${tmp}/${rand}-set`, {});
-exec(`env > ${tmp}/${rand}-env`, {});
-exec(`echo '${res}' > ${tmp}/${rand}-cmd`, {});
+s.fiber(() => {
+  // Try to fetch Root documen to ensure everything is ok
+  sync((cb) => {
+    nuxeo.repository().fetch('/').then(() => {
+      // nothing to do, server is available
+      cb();
+    }).catch(err => {
+      console.log(`${err.name}: ${err.message}`);
+      process.exit(1);
+    });
+  });
+
+  const args = process.argv.splice(2);
+  const change = args[0];
+  const server = args[1];
+  const res = exec(`${P4CLIENT} -p ${server} describe -s ${change}`, {
+    encoding: 'UTF-8'
+  });
+
+  const lines = res.split(os.EOL);
+  const changedFilesIndex = _(lines).findIndex(line => {
+    return line.match(/^affected files/i);
+  });
+  if (changedFilesIndex <= 0) {
+    // No changed files
+    process.exit(0);
+  }
+
+  const files = _(lines).drop(changedFilesIndex).map(line => {
+    // format is:
+    // //depot/src/main/dasd... //Nuxeo/main/dasd#3 delete
+    var m = line.match(/^(.*)\.\.\. (.+)#(\d+) (?:move\/)?(.+)/i);
+    return m ? {
+      filePath: m[1] || m[2],
+      change: m[3],
+      action: m[4]
+    } : null;
+  }).filter(o => {
+    // Filter not matching lines (blank lines for instance)
+    return o;
+  }).value();
+
+  // Execute synchronously a Operation call per files changed
+  const errs = [];
+  _(files).each((params) => {
+    sync((cb) => {
+      nuxeo.operation(OPERATION)
+        .params(params)
+        .execute({
+          headers: {
+            'X-NXVoidOperation': true
+          }
+        })
+        .then(() => {
+          console.log(`INFO: ${params.filePath}#${params.change} ${params.action} correctly pushed to Nuxeo.`);
+          cb();
+        })
+        .catch((err) => {
+          errs.push({
+            error: err,
+            params: params
+          });
+          cb();
+        });
+    });
+  });
+
+  _(errs).each((err) => {
+    console.log(err);
+  });
+});
+
+function sync(func) {
+  return s.await(func(s.defer()));
+}
